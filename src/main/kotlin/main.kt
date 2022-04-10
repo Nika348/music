67a06eb16f239508d0dffd03fda85ec7b3367715
Коваль Вероника <veronika99k@yandex.ru>
2022-04-10 17:20:04 +0300

    music

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+const val PRICE = 100
+const val DISCOUNT = 100
+const val DISCOUNT5 = 0.05
+const val MELOMAN = 0.01
+
+fun main(){
+    val minBuy = 1_000
+    val maxBuy = 10_000
+    print("Введите количество песен для покупки: ")
+    val products = readLine()!!.toUInt()
+    println("Покупали ли вы музыку в прошлом месяце? (y/n)")
+    val isMeloman = (readLine()!!.toString()) == "y"
+
+    var totalPrice: Double = PRICE * products.toDouble()
+    if (totalPrice >= maxBuy) {
+        totalPrice *= (1 - DISCOUNT5);
+    } else if (totalPrice >= minBuy) {
+        totalPrice -= DISCOUNT
+    }
+
+    if (isMeloman) {
+        totalPrice *= (1 - MELOMAN);
+    }
+
+    print("Total price $totalPrice RUB")
+}
